6e76510250b89d1aeb048e8300dd36c0e12ebca6
Nuno Rafael Rocha <nunorafaelrocha@gmail.com>
2014-08-30 01:24:21 +0100

    Chuck-Norris-Joke-Gun

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 // AnguarJS APP
 var app = angular.module('cnjgApp', [])
   .controller('homeCtr', function ($scope, $http) {
-    $scope.current_joke = "aol";
+    
+    // the sound
+    var sound = new Audio('sounds/High_Definition_Machine_gun.mp3');
+    
+    // the joke
+    $scope.current_joke = "";
     
     // get new joke function
     $scope.getNewJoke = function () {
+      
+      sound.play();
       
       $http.get('http://api.icndb.com/jokes/random')
         .success(function (response) {
           
           if (response.type === "success") {
-            console.log(response.value.name);
-            $scope.current_joke = response.value.name;
-            
+            $scope.current_joke = response.value.joke;            
           }
           
         });
