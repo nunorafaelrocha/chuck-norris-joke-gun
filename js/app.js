f4f2735dbea4c636acbda70995d6ebdd44b623b8
Rafael Rocha <nrr@seegno.com>
2014-09-14 22:21:44 +0100

    Add list of previous jokes

--- js/app.js
+++ js/app.js
@@ -1,23 +1,36 @@
 // AngularJS APP
 var app = angular.module('cnjgApp', [])
   .controller('homeCtr', function ($scope, $http) {
-    
+
     // the sound
     var sound = new Audio('sounds/High_Definition_Machine_gun.mp3');
-    
+
     // the joke
     $scope.current_joke = "";
-    
+    // previous jokes
+    $scope.previous_jokes = [];
+
     // get new joke function
+    var is_getnewjoke_locked = false;
     $scope.getNewJoke = function () {
-      
-      sound.play();
-      
-      $http.get('http://api.icndb.com/jokes/random')
-        .success(function (response) {
-          if (response.type === "success") {
-            $scope.current_joke = response.value.joke;            
-          }
-        });
+        if (!is_getnewjoke_locked) {
+            is_getnewjoke_locked = true;
+
+             sound.play();
+
+             $http.get('http://api.icndb.com/jokes/random')
+                .success(function (response) {
+                    if ($scope.current_joke) {
+                        $scope.previous_jokes.push($scope.current_joke);
+                    };
+                    if (response.type === "success") {
+                        $scope.current_joke = response.value.joke;
+                    }
+                    is_getnewjoke_locked = false;
+               })
+                .error(function  () {
+                    is_getnewjoke_locked = false;
+                });
+        };
     };
   });
